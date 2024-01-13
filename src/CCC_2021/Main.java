ec0fa1e3c846573c33181b830acc517d97817028
ioana <ioana.antonia.m@gmail.ca>
2024-01-12 19:20:41 -0500

    Completion of J1 2021

---
+++ src/CCC_2021/Main.java
@@ -0,0 +1,9 @@
+package CCC_2021;
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        var j1 = new J1();
+        j1.calcOutputs();
+    }
+}
